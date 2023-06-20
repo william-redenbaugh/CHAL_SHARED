2df8a1e415d747bb659fbe836e8d214c2d9d112d
William Redenbaugh <wmredenbaugh@gmail.com>
2023-06-20 12:06:43 -0700

    ledmatrix wrapper function

---
+++ csal_ledmatrix.cpp
@@ -0,0 +1,61 @@
+#include "csal_ledmatrix.h"
+
+int os_init_ledmatrix(os_ledmatrix_init_t matrix_init, os_ledmatrix_t *matrix){
+    if(matrix == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    // Basic checking of the structure to make sure none of the parameters are off
+    if( matrix_init.init_func == NULL       | 
+        matrix_init.setpixel_func == NULL   | 
+        matrix_init.update_fun == NULL      | 
+        matrix_init.matrix_ptr == NULL      | 
+        matrix_init.height == 0             |
+        matrix_init.width == 0){
+
+        return OS_RET_INVALID_PARAM;        
+    }
+
+    matrix->init_func = matrix_init.init_func;
+    matrix->setpixel_func = matrix_init.setpixel_func;
+    matrix->update_fun = matrix_init.update_fun;
+    matrix->height = matrix_init.height;
+    matrix->width = matrix_init.width;
+    matrix->data_ptr = matrix_init.matrix_ptr;
+
+    // Calls the initialization function
+    return matrix->update_fun(matrix->data_ptr);
+}
+
+int os_setpixel_ledmatrix(os_ledmatrix_t *matrix, int x, int y, rgb_t rgb){
+    if(matrix == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    if(x > matrix->width | y > matrix->height){
+        return OS_RET_INVALID_PARAM;
+    }
+
+    return matrix->setpixel_func(matrix->data_ptr, x, y, rgb);
+}
+
+int os_setpixel_ledmatrix_hsv(os_ledmatrix_t *matrix, int x, int y, hsv_t hsv){
+    if(matrix == NULL){
+        return OS_RET_NULL_PTR;
+    }
+    
+    if(x > matrix->width | y > matrix->height){
+        return OS_RET_INVALID_PARAM;
+    }
+    rgb_t rgb = hsv2rgb(hsv);
+
+    return matrix->setpixel_func(matrix->data_ptr, x, y, rgb);
+}
+
+int os_ledmatrix_update(os_ledmatrix_t *matrix){
+    if(matrix == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    return matrix->update_fun(matrix->data_ptr);
+}
---
+++ csal_ledmatrix.h
@@ -0,0 +1,93 @@
+#ifndef _CSAL_LEDMATRIX_H
+#define _CSAL_LEDMATRIX_H
+
+#include "global_includes.h"
+#include "color_conv.h"
+
+/**
+ * @brief function pointer for initializing a led matrix
+ * @param void *ptr to whatever data struct holding the actual ledmatrix implementation data
+ * @param int width
+ * @param int height
+*/
+typedef int (*os_init_ledmatrix_ptr)(void *ptr, int width, int height);
+
+/**
+ * @brief Function pointer to set a specific pixel to a specific color
+ * @param void *ptr to whatever data struct holding the actual ledmatrix implementation data
+ * @param int x pos
+ * @param int y pos
+ * @param rgb_t col color
+*/
+typedef int (*os_matrix_setpixel_ptr)(void *ptr,int x, int y, rgb_t col);
+
+/**
+ * @brief Function pointer to update the led matrix
+ * @param void *ptr to whatever data struct holding the actual ledmatrix implementation data
+*/
+typedef int (*os_matrix_update_ptr)(void *ptr);
+
+/**
+ * @brief Populate these with the relevant matrix update commands
+ * @param os_init_ledmatrix_ptr init_func: pointer to function that will initialize the led matrix
+ * @param os_matrix_setpixel_ptr setpixel_func: pointer to the function that will set
+ * @param os_matrix_update_ptr update fun: Function that updates the led matrix to the specific color
+ * @param int width width of led matrix
+ * @param int height height of led matrix
+ * @param void *matrix_ptr pointer to data structure used for whatever eld matrix
+*/
+typedef struct os_ledmatrix_init{
+    os_init_ledmatrix_ptr init_func;
+    os_matrix_setpixel_ptr setpixel_func;
+    os_matrix_update_ptr update_fun;    
+
+    int width; 
+    int height;
+    void *matrix_ptr;
+}os_ledmatrix_init_t;
+
+/**
+ * @brief LED matrix handler
+*/
+typedef struct os_ledmatrix{
+    os_init_ledmatrix_ptr init_func;
+    os_matrix_setpixel_ptr setpixel_func;
+    os_matrix_update_ptr update_fun;
+
+    void *data_ptr;
+    int width; 
+    int height;
+}os_ledmatrix_t;
+
+/**
+ * @brief Initialize matrix
+ * @param os_ledmatrix_init_t matrix initialization structure
+ * @param os_ledmatrix_t *matrix that we want to initialize
+*/
+int os_init_ledmatrix(os_ledmatrix_init_t matrix_init, os_ledmatrix_t *matrix);
+
+/**
+ * @brief Set pixel ledmatrix
+ * @param os_ledmatrix_t *matrix that we want to initialize
+ * @param int x pos
+ * @param int y pos
+ * @param rgb_t col color
+*/
+int os_setpixel_ledmatrix(os_ledmatrix_t *matrix, int x, int y, rgb_t rgb);
+
+/**
+ * @brief Set pixel ledmatrix to specific hsv value
+ * @param os_ledmatrix_t *matrix that we want to initialize
+ * @param int x pos
+ * @param int y pos
+ * @param hsv_t col color
+*/
+int os_setpixel_ledmatrix_hsv(os_ledmatrix_t *matrix, int x, int y, hsv_t hsv);
+
+/**
+ * @brief Updates the ledmatrix
+ * @param os_ledmatrix_t *matrix that we want to initialize
+*/
+int os_ledmatrix_update(os_ledmatrix_t *matrix);
+
+#endif
