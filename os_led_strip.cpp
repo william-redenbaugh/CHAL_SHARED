96437e0b3816e3d112702a6af6adce0f6926db94
William Redenbaugh <wmredenbaugh@gmail.com>
2023-09-05 16:07:38 -0700

    adding support for RMT, changing the way we allow enabled or disabled led strips

--- os_led_strip.cpp
+++ os_led_strip.cpp
@@ -1,6 +1,61 @@
 #include "os_led_strip.h"
+#include "global_includes.h"
 
 #ifdef OS_LED_STRIP
+int os_led_strip_init(os_led_strip_t *strip, led_strip_type_t type, int bus, int gpio, uint32_t numpixels)
+{
+    if (strip == NULL)
+    {
+        return OS_RET_NULL_PTR;
+    }
+
+    switch (type)
+    {
+#ifdef NEOPIXEL_LED_STRIP
+    case STRIP_NEOPIXEL_RGB:
+        strip->strip = _neopixel_os_led_strip_init(bus, gpio, numpixels);
+        strip->strip_set_func = _neopixel_os_led_strip_set;
+        strip->strip_show_func = _neopixel_os_led_strip_show;
+        break;
+#endif
+
+#ifdef LED_STRIP_RMT
+    case STRIP_RMT_RGB:
+        strip->strip = _rmt_os_led_strip_init(bus, gpio, numpixels);
+        strip->strip_set_func = _rmt_os_led_strip_set;
+        strip->strip_show_func = _rmt_os_led_strip_show;
+        break;
+#endif
+    }
+
+    if (strip->strip == NULL)
+    {
+        return OS_RET_LOW_MEM_ERROR;
+    }
+
+    return OS_RET_OK;
+}
+
+// These are essentially wrapper functions since we can change the type of LED strip we want to use
+int os_led_strip_set(os_led_strip_t *strip, uint32_t pixel, uint8_t r, uint8_t g, uint8_t b)
+{
+    if (strip == NULL)
+    {
+        return OS_RET_NULL_PTR;
+    }
+    return strip->strip_set_func(strip->strip, pixel, r, g, b);
+}
+
+int os_led_strip_show(os_led_strip_t *strip)
+{
+
+    if (strip == NULL)
+    {
+        return OS_RET_NULL_PTR;
+    }
+
+    return strip->strip_show_func(strip->strip);
+}
 
 int os_led_strip_set_rgb(os_led_strip_t *strip, uint32_t pixel, rgb_t col)
 {
--- os_led_strip.h
+++ os_led_strip.h
@@ -4,6 +4,123 @@
 #include "color_conv.h"
 #include "enabled_modules.h"
 #ifdef OS_LED_STRIP
+struct _os_led_strip_t;
+
+// All the function definitions for the nepixel strips are here.
+#ifdef NEOPIXEL_LED_STRIP
+/**
+ * @brief Initializes a NeoPixel LED strip object.
+ *
+ * This function initializes a NeoPixel LED strip object with the specified parameters.
+ *
+ * @param bus The communication bus identifier (e.g., SPI or I2C).
+ * @param gpio The GPIO pin number to which the NeoPixel data line is connected.
+ * @param numpixels The number of pixels in the LED strip.
+ * @return A pointer to the initialized NeoPixel LED strip object (_os_led_strip_t*).
+ */
+_os_led_strip_t *_neopixel_os_led_strip_init(int bus, int gpio, uint32_t numpixels);
+
+/**
+ * @brief Sets the color of a specific pixel in the NeoPixel LED strip.
+ *
+ * This function sets the color of a specific pixel in the NeoPixel LED strip.
+ *
+ * @param strip A pointer to the NeoPixel LED strip object.
+ * @param pixel The index of the pixel whose color you want to set.
+ * @param r The red component of the pixel's color (0-255).
+ * @param g The green component of the pixel's color (0-255).
+ * @param b The blue component of the pixel's color (0-255).
+ * @return 0 on success, or a negative error code on failure.
+ */
+int _neopixel_os_led_strip_set(_os_led_strip_t *strip, uint32_t pixel, uint8_t r, uint8_t g, uint8_t b);
+
+/**
+ * @brief Shows the current pixel colors on the NeoPixel LED strip.
+ *
+ * This function sends the color data to the NeoPixel LED strip, making the
+ * colors set by _neopixel_os_led_strip_set() visible on the strip.
+ *
+ * @param strip A pointer to the NeoPixel LED strip object.
+ * @return 0 on success, or a negative error code on failure.
+ */
+int _neopixel_os_led_strip_show(_os_led_strip_t *strip);
+#endif
+
+// When we are using the Remote Control Module(typically for ESP32 to control LED strips)
+#ifdef LED_STRIP_RMT
+/**
+ * @brief Initializes an LED strip object using the Remote Control Module (RMT).
+ *
+ * This function initializes an LED strip object for controlling LED strips using
+ * the Remote Control Module (RMT), typically for ESP32.
+ *
+ * @param strip A pointer to the LED strip object to be initialized.
+ * @param bus The communication bus identifier (e.g., SPI or I2C).
+ * @param gpio The GPIO pin number to which the LED strip data line is connected.
+ * @param numpixels The number of pixels in the LED strip.
+ * @return 0 on success, or a negative error code on failure.
+ *
+ * @note This function is available only when the preprocessor macro LED_STRIP_RMT is defined.
+ */
+_os_led_strip_t *_rmt_os_led_strip_init(int bus, int gpio, uint32_t numpixels);
+
+/**
+ * @brief Sets the color of a specific pixel in the LED strip.
+ *
+ * This function sets the color of a specific pixel in the LED strip.
+ *
+ * @param strip A pointer to the LED strip object.
+ * @param pixel The index of the pixel whose color you want to set.
+ * @param r The red component of the pixel's color (0-255).
+ * @param g The green component of the pixel's color (0-255).
+ * @param b The blue component of the pixel's color (0-255).
+ * @return 0 on success, or a negative error code on failure.
+ *
+ * @note This function is available only when the preprocessor macro LED_STRIP_RMT is defined.
+ */
+int _rmt_os_led_strip_set(_os_led_strip_t *strip, uint32_t pixel, uint8_t r, uint8_t g, uint8_t b);
+
+/**
+ * @brief Shows the current pixel colors on the LED strip.
+ *
+ * This function sends the color data to the LED strip, making the colors set by
+ * _rmt_os_led_strip_set() visible on the strip.
+ *
+ * @param strip A pointer to the LED strip object.
+ * @return 0 on success, or a negative error code on failure.
+ *
+ * @note This function is available only when the preprocessor macro LED_STRIP_RMT is defined.
+ */
+int _rmt_os_led_strip_show(_os_led_strip_t *strip);
+#endif
+
+typedef enum
+{
+#ifdef NEOPIXEL_LED_STRIP
+    STRIP_NEOPIXEL_RGB,
+#endif
+#ifdef LED_STRIP_RMT
+    STRIP_RMT_RGB
+#endif
+} led_strip_type_t;
+
+/**
+ * @brief Pointer Function for setting a led strip
+ */
+typedef int (*led_strip_set_t)(_os_led_strip_t *strip, uint32_t pixel, uint8_t r, uint8_t g, uint8_t b);
+typedef int (*led_strip_show_t)(_os_led_strip_t *strip);
+
+typedef struct os_led_strip_t
+{
+    _os_led_strip_t *strip;
+    os_mut_t mutex;
+    int numpixel;
+
+    // Function pointers for setting and showing the pixel data.
+    led_strip_set_t strip_set_func;
+    led_strip_show_t strip_show_func;
+
+} os_led_strip_t;
 
 /**
  * @brief Initializes the LED strip.
